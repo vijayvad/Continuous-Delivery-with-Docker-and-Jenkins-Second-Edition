85a35589f40c3236245e42ede2b04e089ed7d838
vijayvad <50672817+vijayvad@users.noreply.github.com>
2023-02-17 23:37:31 -0500

    comments added

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+* calculator class
+*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
